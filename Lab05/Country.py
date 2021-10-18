617afe719c05c3c4e84681f775604bc8884fe90d
Tấn Lĩnh <tanlinh15@gmail.com>
2021-10-18 12:18:00 +0700

    update1

--- Lab05/Country.py
+++ Lab05/Country.py
@@ -1,11 +1,23 @@
+#cau a:
 class Country:
     def __init__(self, name, population, area):
         self.name = name
         self.population = population
         self.area = area
-
-    def is_larger(self, objCountry):
-        if self.arena > objCountry.area:
-            return True
+#cau b:
+    def is_larger(self, other):
+        if isinstance(other,Country):
+            return self.area > other.area
         else:
-            return False
+            print("Both objects need to be from class Country")
+            return -1
+#cau c:   
+    def population_density(self):
+        return (self.population / self.area)
+#cau d:
+    def __str__(self):
+        return ("{0} has a population of {1} and its area is {2} square kilometers.".format(self.name,self.population,self.area))
+#cau e:    
+    def __repr__(self):
+        # Sử dụng self.__class__.__name__ to get the object's class name, it returns 'str'
+        return "{0}({1},{2},{3})".format(self.__class__.__name__,self.name,self.population,self.area)
