791651b3e4ba037b022c3166a0e26a3a9bfbf7f0
Tấn Lĩnh <73029689+TanLinh15@users.noreply.github.com>
2021-11-03 23:49:33 +0700

    Update get_api_youtube.py

--- get_picture/get_api_youtube.py
+++ get_picture/get_api_youtube.py
@@ -1,6 +1,6 @@
 import requests
 
-API_KEY = 'AIzaSyALrKc3-W0u_Ku-J2OpyjnqFhV5wKlwKGs'
+API_KEY =''
 video_id= ['7cmvABXyUC0']
 url = "https://www.googleapis.com/youtube/v3/video?id="+video_id+"&part=statistic&key="+API_KEY
 response = requests.get(url).json()
