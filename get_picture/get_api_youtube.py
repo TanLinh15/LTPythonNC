890c756a1466262d294a5a21882a8fc0fb187607
Tấn Lĩnh <tanlinh15@gmail.com>
2021-11-10 09:09:20 +0700

    push bai 4 va 7

--- get_picture/get_api_youtube.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-API_KEY =''
-video_id= ['7cmvABXyUC0']
-url = "https://www.googleapis.com/youtube/v3/video?id="+video_id+"&part=statistic&key="+API_KEY
-response = requests.get(url).json()
-print(response)
